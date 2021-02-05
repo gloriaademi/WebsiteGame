32687eb1be394eac11167ef7f484005c6061f81e
gloriaademi <70911806+gloriaademi@users.noreply.github.com>
2021-02-05 16:30:15 +0100

    Final

---
+++ Projekti-GamesWebsite/Controllers/checkoutController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projekti_GamesWebsite.Controllers
+{
+    public class checkoutController : Controller
+    {
+        // GET: checkout
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult checkout()
+        {
+            return View();
+        }
+    }
+}
